17a06cbe2b5a8177150c510c918fbcf7a14780d6
v_bonchevski <v.bonchevski@valteodigital.com>
2019-08-28 19:30:42 +0300

    Styled collections , added collection-items

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 import './collection-preview.styles.scss';
 
+import CollectionItem from '../collection-item/collection-item.component';
+
 const CollectionPreview = ({title,items}) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
                 // filter to make sure there's only 4 items rendered upon mapping 
-                items.filter((item, idx)=> idx<4).map((item)=>(
-                    <div key={item.id}>{item.name}</div>
+                items.filter((item, idx)=> idx<4)
+                .map(({id, ...otherItemProps})=>(
+                    <CollectionItem key={id} {...otherItemProps} />
                 ))
             }
         </div>
